554c2db2182c36637e36d289316a1bf9e6628f05
solar <spidervvv@mail.ru>
2021-10-01 23:55:47 +0300

    added content of css

---
+++ script.js
@@ -0,0 +1,11 @@
+// for input range
+document.documentElement.classList.add('js');
+addEventListener(
+  'input',
+  (e) => {
+    let _t = e.target;
+    _t.style.setProperty('--val', +_t.value);
+  },
+  false
+);
+/////////////////////////
